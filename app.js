9eee82ebab24b3d91b556564616e7d25ab8fa56a
Babbins <alimahmo@buffalo.edu>
2016-08-08 16:27:09 -0400

    Initialized database

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var swig = require('swig');
 var path = require('path');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-
+var models = require('./models/index.js');
 var app = express();
 
 app.engine('html', swig.renderFile);
@@ -25,6 +25,13 @@ app.get('/', function(req, res, next) {
     res.render('index');
 });
 
-var server = app.listen(3000, function() {
-    console.log('listening on port: 3000');
-});
+models.User.sync({})
+    .then(function(){
+        return models.Page.sync({})
+    })
+    .then(function() {
+        app.listen(3000, function() {
+            console.log('listening on port: 3000');
+        })
+    })
+    .catch(console.error);
--- models/index.js
+++ models/index.js
@@ -1,11 +1,53 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack',{
+  logging: false
+});
 
-var Page = db.define('page', {
+var Page = db.define('page',
+  {
     title: {
-        type: Sequalize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
     },
     urlTitle: {
-        type: Sequalize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    status: {
+      type: Sequelize.ENUM('open', 'closed'),
+    },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
     }
-})
+  },
+  {
+    getterMethods: {
+      route: function() {
+        return '/wiki/' + this.urlTitle;
+      }
+    }
+  }
+);
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isEmail: true
+    }
+
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
