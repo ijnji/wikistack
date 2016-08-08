4d86e029690e31dbaf9f11bc5bc83fc60ec7f63e
Babbins <alimahmo@buffalo.edu>
2016-08-08 18:38:20 -0400

    Form pushes to db

--- models/index.js
+++ models/index.js
@@ -3,6 +3,17 @@ var db = new Sequelize('postgres://localhost:5432/wikistack',{
   logging: false
 });
 
+var generateUrlTitle = function (title) {
+  if (title) {
+    // Removes all non-alphanumeric characters from title
+    // And make whitespace underscore
+    return title.replace(/\s+/g, '_').replace(/\W/g, '');
+  } else {
+    // Generates random 5 letter string
+    return Math.random().toString(36).substring(2, 7);
+  }
+};
+
 var Page = db.define('page',
   {
     title: {
@@ -33,7 +44,9 @@ var Page = db.define('page',
     }
   }
 );
-
+Page.hook('beforeValidate', function(page){
+  page.urlTitle = generateUrlTitle(page.title);
+});
 var User = db.define('user', {
     name: {
         type: Sequelize.STRING,
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,5 +1,7 @@
 var express = require('express');
-
+var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 module.exports = function wikiRouter() {
     var router = express.Router();
 
@@ -9,8 +11,19 @@ module.exports = function wikiRouter() {
     });
 
     router.post('/', function(req, res, next) {
-        console.log(req.body);
-        res.send('got to POST /wiki/');
+        var author = req.body.author;
+        var email = req.body.email;
+        var title = req.body.title;
+        var content = req.body.content;
+        var status = req.body.status;
+        var page = Page.build({
+          title: title,
+          content: content,
+          status: status
+        });
+        page.save().then(function(){
+          res.redirect('/');
+        });
     });
 
     router.get('/add', function(req, res, next) {
